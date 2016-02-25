136013a26f4fbea2f9f63a648bd18710dd54f9d3
Calvin Henry <calvinh@localhost.localdomain>
2016-02-25 18:20:47 -0500

    Added implementation of reading all the emails.

--- mailProject.py
+++ mailProject.py
@@ -4,11 +4,11 @@ import imaplib
 
 
 
-global fromaddr, username, password, server, inbox, readServer
+global fromaddr, username, password, server, inbox, readServer, lastEmail
 
 
 def intializeGlobals():
-    global fromaddr, username, password, server
+    global fromaddr, username, password, server, lastEmail
     fromaddr = 'Person'
     username = 'pythonemailprogramtest@gmail.com'
     password = 'Isitameatpieorafruitpie?'
@@ -17,18 +17,39 @@ def intializeGlobals():
                  ('imap.gmail.com',993)
     readServer.login(username,password)
 
-    inbox,cnt = readServer.select('Inbox')
-
-    inbox, dta = readServer.fetch\
-                 (cnt[0],\
-                  '(UID BODY[TEXT])')
-
-    print dta[0][1]
-
+    sucess, mailboxes = readServer.list();
+    sucess,inbox = readServer.select('PhoneStuff')
+    #Note to self, make sure you test that this will not get mail from the inbox, just PhoneStuff
+    sucess, dta = readServer.search(None, '(UNSEEN)')
+    print sucess
+    if sucess == 'OK' or  len(dta) < 1:
+        print len(dta)
+        for num in dta[0].split(' '):
+            
+            try :
+                sucess, data = readServer.fetch(num,'(RFC822)')
+                text = data[0][1]
+                print parseString(text)
+                typ, data = readServer.store(num,'+FLAGS','\\Seen')
+            except:
+                print 'No Unread Email'
+            
+            
+    else:
+        print "No read emails"
+    #numOfMessages = dta[0][1]
+    #print (numOfMessages)
+#    text = dta[0]
+#    print 'parsedVal'
+#    print text
+#    print 'parsed val length'
+#    print len(text)
     readServer.close()
     readServer.logout()
     
     return;
+                                       
+
 
 def sendEmail(recipient, subject, email):
     global fromaddr, username, password, server
@@ -44,7 +65,24 @@ def sendEmail(recipient, subject, email):
 
 def checkMail():
 
+    
+    
+   
+    
     return;
+
+
+def parseString(inFrom):
+    print(inFrom);
+    front = 'Content-Location: text_0.txt'
+    backExtraConst = -2;
+    frontExtraConst = 4
+    back = '--__CONTENT_64564_PART_BOUNDARY__33243242__--'
+    
+    parse = inFrom[inFrom.find(front)+ len(front)+ frontExtraConst:\
+                   inFrom.find(back)+ backExtraConst]
+
+    return parse;
 
 
 intializeGlobals()
@@ -56,4 +94,5 @@ parser.add_argument('--email', '-m')
 args = parser.parse_args()
 #print(inbox)
 #sendEmail(args.recipient, args.subject, args.email)
+
 
